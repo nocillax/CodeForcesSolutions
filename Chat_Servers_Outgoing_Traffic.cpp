6496fa4ac2ec12ff6de00f7092bd674eeb06f7a3
Md Asif Chowdhury <asifjarif@gmail.com>
2024-01-06 07:14:17 +0600

    Create Chat_Servers_Outgoing_Traffic.cpp

---
+++ Chat_Servers_Outgoing_Traffic.cpp
@@ -0,0 +1,79 @@
+// ------------------------------------------------------------------------------
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <set>
+using namespace std;
+// ------------------------------------------------------------------------------
+typedef unsigned int ui;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+// ------------------------------------------------------------------------------
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define READ          freopen("in.txt","r",stdin)
+#define WRITE         freopen("out.txt","w",stdout);
+#define nl            "\n"
+#define PI            acos(-1.0)
+#define mem(arr,val)  memset(arr,val,sizeof(arr))
+#define pb            push_back
+#define mp            make_pair
+#define mpl           make_pair<ll, ll>
+#define PR            pair<ll,ll>
+#define ff            first
+#define ss            second
+#define mod           10000007
+#define INF           1e18
+#define EPS           1e-2
+// ------------------------------------------------------------------------------
+ 
+ 
+ll traffic = 0, cnt = 0;
+ 
+void NoCiLLaX(){
+ 
+ 
+ 
+    while(1){
+ 
+        string s; getline(cin, s);
+ 
+        if(s.empty()) break;
+ 
+        else if(s[0] == '+'){
+            cnt++;
+            
+        }
+ 
+        else if(s[0] == '-'){
+            cnt--;
+        }
+ 
+        else {
+            ll size;
+            for(ll i = 0; i < s.size(); i++){
+ 
+                if(s[i] == ':'){
+                    
+                    size = (s.size() - 1 - i);
+ 
+                }
+            }
+ 
+            traffic = traffic + ( size * cnt );
+        }
+    }
+ 
+cout << traffic << endl;
+ 
+ 
+}
+ 
+int main(){
+    NoCiLLaX();
+}
